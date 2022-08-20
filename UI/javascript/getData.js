094fb381c40356c2f595f4019f40ca5dc9d33a59
Fahd Khan <Fahdkhan487@gmail.com>
2022-08-20 17:19:59 +0500

    ui pushed

---
+++ UI/javascript/getData.js
@@ -0,0 +1,38 @@
+var jwtToken;
+
+
+function checkLoginCredentials(){
+
+    email = document.getElementById("username").value;
+    password = document.getElementById("password").value;
+
+    var loginCredentials = {
+        email:email,
+        password:password
+    }
+    
+    fetch("http://localhost:8081/login",{
+        body:JSON.stringify(loginCredentials),
+        method:"POST",
+        headers:{
+            "Content-type":"application/json"
+        }
+    })
+    .then((response) => response.json())
+    .then((jwttoken) => jwtToken = jwttoken.jwt )
+    .catch((error)=> console.log(error))
+
+    setTimeout(function(){
+        if(jwtToken != null){
+                window.open(`http://localhost:8080/listing.html?`+qs,"_self")
+        }
+        else{
+            alert("Wrong password")
+        } 
+    },300)
+}
+
+
+
+
+
